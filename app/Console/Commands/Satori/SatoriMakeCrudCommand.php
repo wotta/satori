709f96c8e531ef3d19bd8d71425c2959cc28e960
Wouter van Marrum <wouter.van.marrum@kaartje2go.nl>
2025-08-02 22:14:18 +0200

    feat: test

--- app/Console/Commands/Satori/SatoriMakeCrudCommand.php
+++ app/Console/Commands/Satori/SatoriMakeCrudCommand.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands\Satori;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Command\Command as BaseCommand;
+use function Laravel\Prompts\confirm;
 
 class SatoriMakeCrudCommand extends Command
 {
@@ -10,8 +14,47 @@ class SatoriMakeCrudCommand extends Command
 
     protected $description = 'Command description';
 
-    public function handle(): void
+    public function handle(): int
     {
+        if (! File::exists(base_path('draft.yml'))) {
+            Artisan::call('blueprint:init');
+        }
 
+        if (! confirm('Do you want to run blueprint:build?')) {
+            $this->comment('Stopping script here.');
+
+            return BaseCommand::SUCCESS;
+        }
+
+        $output = Artisan::call('blueprint:build');
+        if ($output === BaseCommand::FAILURE) {
+            $this->error('Blueprint build failed');
+
+            return BaseCommand::FAILURE;
+        }
+
+        $this->info('Blueprint built successfully');
+
+        if (! confirm('Do you want to migrate the database?')) {
+            $this->comment('Stopping script here.');
+
+            return BaseCommand::SUCCESS;
+        }
+
+        $output = Artisan::call('migrate', [
+            '--force' => true,
+            '--no-interaction' => true,
+        ]);
+
+        if ($output === BaseCommand::FAILURE) {
+            $this->error('Migration failed');
+
+            return BaseCommand::FAILURE;
+        }
+
+        $this->newLine(2);
+        $this->info('Creating filament crud');
+
+        return BaseCommand::SUCCESS;
     }
 }
