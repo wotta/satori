26cc417fc27f5b2a710c41eeecb252b5d55cbe9d
Wouter <wouter.van.marrum@protonmail.com>
2025-10-03 22:08:56 +0200

    wip

--- app/Console/Commands/Satori/SatoriInstallCommand.php
+++ app/Console/Commands/Satori/SatoriInstallCommand.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Str;
 use JsonException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command as BaseCommand;
+use Throwable;
 
 use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\suggest;
@@ -142,7 +143,19 @@ final class SatoriInstallCommand extends Command
 
                 return;
             case 'vizra/vizra-adk':
-                $this->call('vizra:install');
+                try {
+                    $this->call('vizra:install');
+                } catch(Throwable) {
+                    $this->error('Could not finish vizra installation.');
+                    $this->newLine(2);
+                    $this->info('Check if prism was installed correctly. If not run the following commands:');
+                    $this->line(str_repeat('<fg=white;bg=gray> </>', 60));
+                    $this->line('<fg=white;bg=gray>    $ composer require '.$packageToInstall.str_repeat(' ', 22).'</>');
+                    $this->line('<fg=white;bg=gray>    $ php artisan vizra:install'.str_repeat(' ', 22).'</>');
+                    $this->line(str_repeat('<fg=white;bg=gray> </>', 60));
+                    $this->newLine(2);
+                }
+
 
                 $this->newLine(2);
                 $this->info('Create your first agent check out the documentation: https://github.com/vizra-ai/vizra-adk?tab=readme-ov-file#-quick-start');
